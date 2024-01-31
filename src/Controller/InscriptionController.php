f19efd8c3e3b43cd26d714ed96234d6060f015e7
Nino Fazer <fazernino@gmail.com>
2024-01-31 20:30:24 +0100

    J'ai finis la page InscriptionController.php

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,6 +32,19 @@ class InscriptionController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Hachez le mot de passe avant de l'enregistrer
+            $hashedPassword = password_hash($user->getPassword(), PASSWORD_DEFAULT);
+            $user->setPassword($hashedPassword);
+
+            $existingUser = $entityManager->getRepository(User::class)->findOneBy(['Email' => $user->getEmail()]);
+            if ($existingUser) {
+                // Ajoutez un message flash pour informer l'utilisateur que cet e-mail existe déjà
+                $this->addFlash('error', 'L\'adresse e-mail existe déjà.');
+
+                // Redirigez l'utilisateur vers la page d'inscription
+                return $this->redirectToRoute('community_support_inscription');
+            }
+
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -46,6 +60,4 @@ class InscriptionController extends AbstractController
             'form' => $form,
         ]);
     }
-
-
 }
