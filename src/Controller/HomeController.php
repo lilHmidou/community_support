3442bbaabc7f5d5e729139e2f783ff86df3abd2b
kboumedine <khera.boumedine@gmail.com>
2024-02-19 19:29:28 +0100

    start of home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+}
