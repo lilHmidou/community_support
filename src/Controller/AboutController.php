f001a76f2c1a9888322f93a3052ac3cdaee79a0b
Layal <layalelzein2@gmail.com>
2024-02-22 00:09:49 +0100

    first part "About Us" page

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutController extends AbstractController
+{
+    #[Route('/about', name: 'about')]
+    public function index(): Response
+    {
+        return $this->render('about/about.html.twig');
+    }
+
+}
