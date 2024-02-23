b5c14a8f35c5a7a5f81b397525735fc11b83d85d
Nino Fazer <fazernino@gmail.com>
2024-02-23 16:43:26 +0100

    back-end Registration

--- assets/controllers/password-toggle.js
+++ assets/controllers/password-toggle.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggleButton');
     const passwordField = document.getElementById('passwordField');
+    const confirmPasswordField = document.getElementById('confirmPasswordField');
 
     toggleButton.addEventListener('click', function() {
-        if (passwordField.type === 'password') {
-            passwordField.type = 'text';
-            toggleButton.innerHTML = '<i class="fa-regular fa-eye-slash"></i>'; // Œil fermé
-        } else {
-            passwordField.type = 'password';
-            toggleButton.innerHTML = '<i class="fa-regular fa-eye"></i>'; // Œil ouvert
-        }
+        togglePasswordFieldVisibility(passwordField, toggleButton);
+        togglePasswordFieldVisibility(confirmPasswordField, toggleButton);
     });
+
+    function togglePasswordFieldVisibility(field, button) {
+        if (field.type === 'password') {
+            field.type = 'text';
+            button.innerHTML = '<i class="fa-regular fa-eye"></i>'; // Œil ouvert
+        } else {
+            field.type = 'password';
+            button.innerHTML = '<i class="fa-regular fa-eye-slash"></i>'; // Œil fermé
+        }
+    }
 });
---
+++ assets/controllers/password-validation.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordField = document.getElementById('passwordField');
+    const criteriaListItems = document.querySelectorAll('.criteria-list li');
+
+    passwordField.addEventListener('input', function() {
+        const password = passwordField.value;
+        criteriaListItems.forEach(item => {
+            const criterion = item.textContent.trim();
+            if (checkCriterion(password, criterion)) {
+                item.classList.add('criteria-met');
+            } else {
+                item.classList.remove('criteria-met');
+            }
+        });
+    });
+
+    function checkCriterion(password, criterion) {
+        switch (criterion) {
+            case 'au moins 8 caractères':
+                return password.length >= 8;
+            case 'une lettre majuscule':
+                return /[A-Z]/.test(password);
+            case 'une lettre minuscule':
+                return /[a-z]/.test(password);
+            case 'un chiffre':
+                return /\d/.test(password);
+            case 'un caractère spécial @ $ ! % * ? & .':
+                return /[@$!%*?&.]/.test(password);
+            default:
+                return false;
+        }
+    }
+});
