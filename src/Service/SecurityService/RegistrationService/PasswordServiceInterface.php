f221554cd17a5bc815b004bc35c2cad2f2ee6620
Nino <fazernino@gmail.com>
2024-04-27 00:33:42 +0200

    refacto password

--- src/Service/SecurityService/RegistrationService/PasswordServiceInterface.php
+++ src/Service/SecurityService/RegistrationService/PasswordServiceInterface.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\FormInterface;
 interface PasswordServiceInterface
 {
     public function getPasswordData(FormInterface $form): array;
-    public function checkPasswordMatch(array $passwordData): bool;
+    public function getNewPasswordData(FormInterface $form): array;
+    public function checkPasswordMatch(array $passwordData, string $type): bool;
     public function hashPassword(User $user, string $plainPassword): string;
+    public function updatePassword(User $user, array $passwordData): void;
 }
